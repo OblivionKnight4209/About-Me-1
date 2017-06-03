21062367dde96b14f04f0124db77112fb1f8a072
Nicholas Welch <nicholaswelch1989@gmail.com>
2017-06-03 15:59:20 -0700

    lab4 chnages made

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # About-Me
 This is a simple website showcasing information about me!
+06/03/2017 Nicholas Welch
+--Made changes to the app.js file. Added in functions. Comment out old code
--- app.js
+++ app.js
@@ -1,21 +1,49 @@
 'use strict';
+function yesQuestions(answer){
+  //var answer =  prompt ('Can pigs fly?');
 
-var question1 = prompt('Do I have siblings? Answer yes or no, y or n');
+  //var answer.toUpperCase() === 'YES' || answer.toUpperCase() === 'Y');---remove
+  //new code
+  if (answer.toUpperCase() === 'YES' || answer.toUpperCase() === 'Y') {
+    alert('Correct! High five buddy!');
+  } else {
+    alert('I thought you were my friend :(');
+  }
+};
+function noQuestions(answer){
+  if (answer.toUpperCase() === 'NO' || answer.toUpperCase() === 'N'){
+    alert('Correct! High five buddy!');
+  } else {
+    alert('I thought you were my friend :(');
+  }
+};
+  yesQuestions(prompt('Do I have siblings? Answer yes or no, y or n'))
+  yesQuestions(prompt('Is crimson my favorite color? Answer yes or no, y or n'))
+  noQuestions(prompt('Did I eat Oatmel for breafast on Tuesday? Answer yes or no, y or n'))
+  noQuestions(prompt('Is Pikachu my highest level Pokemon in Pokemon Crystal? Answer yes or no, y or n'))
+  yesQuestions(prompt('Is Ashley Graham my celebrity crush? Answer yes or no, y or n'))
+  /* old code
+ if (question1.toUpperCase() === 'YES' || question1.toUpperCase() === 'Y') {
+    alert('Correct! High five buddy!');
+  } else {
+    alert('I thought you were my friend :(');
+}*/
+/*-moved into prompt-- var question1 = prompt('Do I have siblings? Answer yes or no, y or n');
 
 if (question1.toUpperCase() === 'YES' || question1.toUpperCase() === 'Y') {
   alert('Correct! High five buddy!');
 } else {
   alert('I thought you were my friend :(');
-};
-var question2 = prompt('Is crimson my favorite color? Answer yes or no, y or n');
+};*/
+/*var question2 = prompt('Is crimson my favorite color? Answer yes or no, y or n');
 
 if (question2.toUpperCase() === 'YES' || question2.toUpperCase() === 'Y') {
   alert('Correct! I bleed Crimson. GO COUGS!');
 } else {
   alert('I thought you were my friend :(');
-};
+};*/
 
-var question3 = prompt('Did I eat Oatmel for breafast on Tuesday? Answer yes or no, y or n');
+/*var question3 = prompt('Did I eat Oatmel for breafast on Tuesday? Answer yes or no, y or n');
 
 if (question3.toUpperCase() === 'NO' || question3.toUpperCase() === 'N') {
 alert('Correct! I ate donuts!');
@@ -38,21 +66,22 @@ alert('Correct! She is very beautiful!');
 } else {
 alert('I thought you were my friend :(');
 };
-
+*/
 
 document.write('<h2> Do I have siblings? Yes!</h2>');
 document.write('<h2>Is crimson my favorite color? Yes!</h2>');
 document.write('<h2>Did I eat Oatmel for breafast on Tuesday? No!</h2>');
 document.write('<h2>Is Pikachu my highest level Pokemon in Pokemon Crystal? No!</h2>');
 document.write('<h2>Is Ashley Graham my celebrity crush? Yes!</h2>');
-document.write('<h1> </h1>');
-document.write('<h1>How well did you do: </h1>');
+/*document.write('<h1> </h1>');
+//document.write('<h1>How well did you do: </h1>');
 
 
-var outcome = question1.length + question2.length + question3.length + question4.length + question5.length
+//var outcome = question1.length + question2.length + question3.length + question4.length + question5.length
 
 if (outcome > 12) {
 document.write('You are my friend!');
 } else {
 document.write('I hate you.');
 };
+*/
